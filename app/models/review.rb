fac0eff3fe1dbaf4b9fe55e2a8ea2318f7ae30f8
valeria zambrano <Valeria.zambrano.guzman@gmail.com>
2021-07-26 17:13:02 -0400

    Rails yelp mvp finished

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
     belongs_to :restaurant
+    validates :rating, inclusion: { in: (0..5)}, numericality: { only_integer: true }
+    #numericality, mayor que o menor que
+    validates :content, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,15 @@ puts "Cleaning database..."
 Restaurant.destroy_all
 
 puts "Creating restaurants..."
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "french" }
-pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
-chinese_xi = { name: "Xia Lin", address: "650 Green St, London", category: "chinese" }
-belgian_bu = { name: "Rest Belgian", address: "78 Blue St, London", category: "belgian" }
-japan_food = { name: "Nokogiri", address: "780 Gem St, London", category: "japanese" }
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "1", category: "french" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "2", category: "italian" }
+chinese_xi = { name: "Xia Lin", address: "650 Green St, London", phone_number: "3", category: "chinese" }
+belgian_bu = { name: "Rest Belgian", address: "78 Blue St, London", phone_number: "4", category: "belgian" }
+japan_food = { name: "Nokogiri", address: "780 Gem St, London", phone_number: "5", category: "japanese" }
 
 [ dishoom, pizza_east, chinese_xi, belgian_bu, japan_food ].each do |attributes|
   restaurant = Restaurant.create!(attributes)
   puts "Created #{restaurant.name}"
 end
 puts "Finished!"
+2
